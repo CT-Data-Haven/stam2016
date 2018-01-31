ee2c27d7f9d36c6945bbd124edcb1a8dbc55bbe7
camille-s <camille.seaberry@gmail.com>
2018-01-31 18:37:34 -0500

    Fix SUI files

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import './semantic-ui-blue/semantic.min.css';
+import 'semantic-ui-css/semantic.min.css';
+// import './semantic-ui-blue/semantic.min.css';
 
 import { loadData } from './components/DataLoader';
 
